e4217369af36c3191a951e2b6d53a745e7994a33
System administrator <root@nixos.localdomain>
2026-03-30 21:39:16 +0000

    yazi

--- home.nix
+++ home.nix
@@ -16,6 +16,15 @@ in
     extraConfig = ''
       $env.config.show_banner = false
       alias fg = job unfreeze
+      def --env y [...args] {
+        let tmp = (mktemp -t "yazi-cwd.XXXXXX")
+        yazi ...$args --cwd-file $tmp
+        let cwd = (open $tmp)
+        if $cwd != "" and $cwd != $env.PWD {
+          cd $cwd
+        }
+        rm -f $tmp
+      }
     '';
   };
 
